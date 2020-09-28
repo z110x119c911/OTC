f324c3ef8cedfcf5fcbebb09d92296cbad368dd2
james HE <james840525@gmail.com>
2020-09-28 15:32:43 +0800

    v1

---
+++ source/js/all.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    // $('a').click(function(e){
+    //     event.preventDefault();
+    // })
+    $('.scroll').click(function () {
+        let target = $(this.hash);
+        $('html,body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    });
+    $('.scrollTop').click(function (e) {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    });
+    $(window).scroll(function () {
+        let scroll = $(window).scrollTop();
+        if(scroll >= 50){
+            $('.scrollTop').addClass("show");
+        }else{
+            $('.scrollTop').removeClass("show");
+        }
+    });
+    // $(window).scroll(function () {
+    //     var scrollPos = $(window).scrollTop();
+    //     var windowHeight = $(window).height();
+    //     $('.animate__animated').each(function () {
+    //         var thisPos = $(this).offset().top;
+    //         console.log(thisPos);
+    //         if (windowHeight * 0.7 + scrollPos >= (thisPos)) {
+    //             $(this).removeClass('d-none');
+    //             $(this).addClass('animate__fadeInUp');
+                
+    //         }
+    //     });
+    // });
+});
